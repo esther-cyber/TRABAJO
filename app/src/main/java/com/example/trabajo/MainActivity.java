f8f5123aebab842fc765c8f3c23793bc36919d80
esther-cyber <estherdelsastre@gmail.com>
2021-12-27 13:49:35 +0100

    commit

--- app/src/main/java/com/example/trabajo/MainActivity.java
+++ app/src/main/java/com/example/trabajo/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.trabajo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button comenzar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        comenzar = (Button) findViewById(R.id.boton1);
+
+        comenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent comenzar = new Intent(MainActivity.this, PantallaFrecuencias.class);
+                startActivity(comenzar);
+
+            }
+        });
     }
+
 }

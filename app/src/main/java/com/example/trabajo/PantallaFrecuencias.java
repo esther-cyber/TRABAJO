096d12be712a40983782c9ee0da89dfcce1484da
esther-cyber <estherdelsastre@gmail.com>
2021-12-28 13:23:02 +0100

    commit

--- app/src/main/java/com/example/trabajo/PantallaFrecuencias.java
+++ app/src/main/java/com/example/trabajo/PantallaFrecuencias.java
@@ -4,13 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class PantallaFrecuencias extends AppCompatActivity {
+
+    Button altas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla_frecuencias);
+
+        altas = (Button) findViewById(R.id.boton2);
+
+        altas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent altas = new Intent(PantallaFrecuencias.this, Pantalla_Altas.class);
+                startActivity(altas);
+
+            }
+        });
     }
 }
